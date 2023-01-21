06eb78f82c7d28b7eb53524fc768f0196cc3de7f
Aninda <105064729+dasaninda@users.noreply.github.com>
2023-01-20 20:46:05 -0500

    Add files via upload

--- day 20/snake.py
+++ day 20/snake.py
@@ -6,27 +6,33 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
 
     def __init__(self):
-        self.segment = []
+        self.segments = []
         self.create_snake()
-        self.head = self.segment[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for position in STARTING_POSITION: 
-            tim = Turtle("square")
-            tim.color("white")
-            tim.penup()
-            tim.goto(position)
-            self.segment.append(tim)
-            
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
-        for seg_num in range (len(self.segment) -1, 0, -1):
-            new_x = self.segment[seg_num -1].xcor
-            new_y = self.segment[seg_num -1].ycor
-            self.segment[seg_num].goto(new_x, new_y)
-    
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
@@ -34,13 +40,13 @@ class Snake:
             self.head.setheading(UP)
 
     def down(self):
-         if self.head.heading() != UP:
+        if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-         if self.head.heading() != RIGHT:
+        if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-         if self.head.heading() != LEFT:
+        if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
